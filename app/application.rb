cf1ee42d7ab30cb2c4014290d6de39dedc436ad5
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-18 16:41:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,7 +23,7 @@ class Application
         end
       end
     elsif req.path.match(/add/)
-      add_item = req.params[params["q"]
+      add_item = req.params["q"]
     else
       resp.write "Path Not Found"
     end

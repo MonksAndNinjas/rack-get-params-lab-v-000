98bb052ddb938e1985a9df29060947187e5d44ab
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-18 16:48:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -33,6 +33,8 @@ class Application
       if @@items.include?(add_item)
         @@cart << add_item
         resp.write "added #{add_item}"
+      else
+        "We don't have that item"
       end
 
     else
